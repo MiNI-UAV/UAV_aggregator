29e30cb358cc2bdb6f4f576104afbb9d578413f9
Wojciech Gajda <wggajda@gmail.com>
2023-07-04 19:07:15 +0200

    shot api

--- src/clients.rs
+++ src/clients.rs
@@ -6,6 +6,7 @@ pub struct Clients
 {
     running: Arc<AtomicBool>,
     _proxies: Arc<Mutex<Vec<Option<thread::JoinHandle<()>>>>>,
+    _control: Arc<Mutex<Vec<Option<thread::JoinHandle<()>>>>>,
     _replyer: Option<thread::JoinHandle<()>>
 }
 
@@ -19,6 +20,8 @@ impl Clients
         let mut taken_name =  HashSet::<String>::new();
         let proxies = Arc::new(Mutex::new(Vec::<Option::<JoinHandle<()>>>::new()));
         let p = proxies.clone();
+        let control = Arc::new(Mutex::new(Vec::<Option::<JoinHandle<()>>>::new()));
+        let c = proxies.clone();
         let replyer: JoinHandle<()> = thread::spawn(move ||
         {
             replyer_socket.set_rcvtimeo(1000).unwrap();
@@ -44,8 +47,9 @@ impl Clients
                     drone_name.push_str(&no.to_string());
                 }
                 taken_name.insert(drone_name.to_string());
-                let mut drones = drones.lock().unwrap();
-                let (drone_no,uav_address) = drones.startUAV(&drone_name);
+                let mut drones_lck = drones.lock().unwrap();
+                let (drone_no,uav_address) = drones_lck.startUAV(&drone_name);
+                drop(drones_lck);
                 println!("Started new drone with name: {}", drone_name);
                 let mut steer_pair_socket = _ctx.socket(zmq::PAIR).unwrap();
                 let address = format!("tcp://127.0.0.1:{}", next_port);
@@ -55,7 +59,6 @@ impl Clients
                 let mut stop_sub_socket = _ctx.socket(zmq::SUB).unwrap();
                 stop_sub_socket.set_subscribe(b"").unwrap();
                 stop_sub_socket.connect("inproc://stop").unwrap();
-
                 let mut proxy = p.lock().unwrap();
                 proxy.push(Some(
                     thread::spawn(move ||
@@ -66,16 +69,59 @@ impl Clients
                     }))
                 );
                 drop(proxy);
-                println!("Ready to connect client on TCP: {}", next_port);
-                let mut reply = String::new();
+                println!("Ready to connect steer client on TCP: {}", next_port);
+
+                let control_pair_socket = _ctx.socket(zmq::PAIR).unwrap();
+                let address = format!("tcp://127.0.0.1:{}", next_port+1000);
+                control_pair_socket.bind(&address).unwrap();
+                let mut control = c.lock().unwrap();
+                let r2 = r.clone();
+                let d2 = drones.clone();
+                control.push(Some(
+                    thread::spawn(move ||
+                    {
+                        control_pair_socket.set_rcvtimeo(1000).unwrap();
+                        let address = format!("tcp://127.0.0.1:{}", next_port+1000);
+                        control_pair_socket.bind(&address).unwrap();
+                        while r2.load(Ordering::SeqCst) {
+                            let mut request =  zmq::Message::new();
+                            if let Err(_) = control_pair_socket.recv(&mut request, 0)
+                            {
+                                continue;
+                            }
+                            let mut d_lck = d2.lock().unwrap();
+                            Clients::handleControlMsg(request.as_str().unwrap(), drone_no, &mut d_lck);
+                            drop(d_lck);
+                        }
+                    })
+                ));
+                drop(control);
+                println!("Ready to connect control client on TCP: {}", next_port+1000);
+
+                let mut reply = String::with_capacity(30);
                 reply.push_str(&drone_no.to_string());
                 reply.push(',');
                 reply.push_str(&next_port.to_string());
+                reply.push(',');
+                reply.push_str(&(next_port+1000).to_string());
                 replyer_socket.send(&reply, 0).unwrap();
                 next_port = next_port + 1;
             }
         });
-        Clients{running: running, _proxies: proxies, _replyer: Some(replyer)}
+        Clients{running: running, _proxies: proxies, _control: control, _replyer: Some(replyer)}
+    }
+
+    fn handleControlMsg(msg: &str, drone_no: usize, drones: &mut Drones)
+    {
+        match msg {
+            "shot" => {
+                if let Some(uav) = drones.drones.get(drone_no)
+                {
+                    uav.dropOrShot(None, None, None, None);
+                }
+            }
+            _ => {}
+        }
     }
 }
 
@@ -93,7 +139,16 @@ impl Drop for Clients{
                 h.join().expect("Proxy joining error");
             }
         }
-        drop(proxy); 
+        drop(proxy);
+        let mut control = self._control.lock().unwrap();
+        while let Some(handler) = control.pop()
+        {
+            if let Some(h) = handler
+            {
+                h.join().expect("Control joining error");
+            }
+        }
+        drop(control); 
         println!("Clients instance dropped");
     }
 }
