292c142a445300e3375964a8e205841e0ce71526
Wojciech Gajda <wggajda@gmail.com>
2023-08-15 19:25:46 +0200

    Tune VZ

--- logs/check.m
+++ logs/check.m
@@ -1,5 +1,5 @@
 clc;clear; close all
-num = 50;
+num = 7;
 ahrs = readmatrix(num2str(num) + "/ahrs.csv");
 env = readmatrix(num2str(num) + "/env.csv");
 ekf = readmatrix(num2str(num) + "/EKF.csv");
@@ -41,29 +41,29 @@ ekf = readmatrix(num2str(num) + "/EKF.csv");
 % legend("ahrs", "env")
 % title("Yaw")
 
-figure(97)
-plot(ahrs(:,1),ahrs(:,5))
-hold on
-plot(ahrs(:,1),ahrs(:,8))
-plot(env(:,1),env(:,5))
-legend("gyro", "acc", "env")
-title("Roll")
-
-figure(98)
-plot(ahrs(:,1),ahrs(:,6))
-hold on
-plot(ahrs(:,1),ahrs(:,9))
-plot(env(:,1),env(:,6))
-legend("gyro", "acc", "env")
-title("Pitch")
-
-figure(99)
-plot(ahrs(:,1),ahrs(:,7))
-hold on
-plot(ahrs(:,1),ahrs(:,10))
-plot(env(:,1),env(:,7))
-legend("gyro", "acc", "env")
-title("Yaw")
+% figure(97)
+% plot(ahrs(:,1),ahrs(:,5))
+% hold on
+% plot(ahrs(:,1),ahrs(:,8))
+% plot(env(:,1),env(:,5))
+% legend("gyro", "acc", "env")
+% title("Roll")
+% 
+% figure(98)
+% plot(ahrs(:,1),ahrs(:,6))
+% hold on
+% plot(ahrs(:,1),ahrs(:,9))
+% plot(env(:,1),env(:,6))
+% legend("gyro", "acc", "env")
+% title("Pitch")
+% 
+% figure(99)
+% plot(ahrs(:,1),ahrs(:,7))
+% hold on
+% plot(ahrs(:,1),ahrs(:,10))
+% plot(env(:,1),env(:,7))
+% legend("gyro", "acc", "env")
+% title("Yaw")
 % 
 % figure(1)
 % plot(ekf(:,1),ekf(:,2))
@@ -78,14 +78,14 @@ title("Yaw")
 % plot(env(:,1),env(:,3))
 % legend("ekf", "env")
 % title("Y")
-% 
-% figure(3)
-% plot(ekf(:,1),ekf(:,4))
-% hold on
-% plot(env(:,1),env(:,4))
-% legend("ekf", "env")
-% title("Z")
-% 
+
+figure(3)
+plot(ekf(:,1),ekf(:,4))
+hold on
+plot(env(:,1),env(:,4))
+legend("ekf", "env")
+title("Z")
+
 % 
 % figure(4)
 % plot(ekf(:,1),ekf(:,5))
@@ -101,10 +101,10 @@ title("Yaw")
 % legend("ekf", "env")
 % title("VY")
 % 
-% figure(6)
-% plot(ekf(:,1),ekf(:,7))
-% hold on
-% plot(env(:,1),env(:,10))
-% legend("ekf", "env")
-% title("VZ")
+figure(6)
+plot(ekf(:,1),ekf(:,7))
+hold on
+plot(env(:,1),env(:,10))
+legend("ekf", "env")
+title("VZ")
 
